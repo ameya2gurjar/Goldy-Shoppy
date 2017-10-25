ed36b6981aaf2309941df344d9e12ced2ea260b4
Pratik Kotwal <pkotwal16@gmail.com>
2017-10-25 01:39:41 -0500

    almost there

--- routes/search.js
+++ routes/search.js
@@ -5,7 +5,7 @@ var moment = require('moment');
 var mongodb = require('mongodb');
 
 router.get('/', function(req, res, next) {
-    var sitem=req.query.sitem;    
+    var sitem=req.query.sitem;
     var category=req.query.category;
     var sortBy=req.query.SortBy;
     var sellType=req.query.SellType;
@@ -18,10 +18,10 @@ router.get('/', function(req, res, next) {
          sortBy={ posted_at : 1 };
     else
         sortBy={ posted_at : -1 };
-    
-    
+
+
     var searchBy;
-    
+
     if(!!!sitem){
         searchBy =  {category:category};
     }else{
@@ -40,17 +40,17 @@ router.get('/', function(req, res, next) {
                 else{
                     searchBy= {$and: [{ $text: { $search: sitem  } },{category:category} ] };
                 }
-                
+
             }
-            
-            
+
+
         }
     }
-    
+
     req.db.collection("products").find(searchBy).sort(sortBy).toArray(function(err, result){
             sendData(result);
     });
-    
+
     function sendData(result){
         var utcMilliSeconds;
         var d = new Date(0);
@@ -58,30 +58,28 @@ router.get('/', function(req, res, next) {
             if(!!result[i].images)
             result[i].img = result[i].images[0];
         }
-        
+
         for(var i in result){
             if(!!result[i].posted_at)
                 {
                     var m = moment(result[i].posted_at).fromNow();
                     result[i].newTime = m;
                     console.log(result[i].newTime);
-                } 
+                }
         }
 //        console.log(utcMilliSeconds);
-        
+
 //        console.log(result);
 //        console.log(result.posted_at);
 //    console.log(result.images);
         res.render('search',{
-        scripts: ['search.js'],    
+        scripts: ['search.js'],
         user: req.user,
         result: result,
-        sitem: sitem    
-        });   
+        sitem: sitem  ,
+        category: category
+        });
     }
 });
 
 module.exports = router;
-
-
-
